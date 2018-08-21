2c5b01d630aefdfc821fa2f5098488960e604696
dajkam <sopotfilip@onet.pl>
2018-08-21 15:51:00 +0200

    początek tworzenia modelu

---
+++ model/User.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 21.08.18
+ * Time: 14:54
+ */
+
+class User
+{
+// privileges
+public  $view = false;
+public  $delete = false;
+public $modify = false;
+
+// login && password
+
+public $login = "";
+public $pass = "";
+
+
+}
